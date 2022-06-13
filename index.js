847a089304bbe76168c02976f720b32921d23e05
Karan Tyagi <karantyagi9702@gmail.com>
2022-06-13 13:08:31 +0530

    Setup rabbitmq

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const axios = require("axios");
 const express = require("express");
 const cors = require("cors");
+const nodemailer = require("nodemailer");
+const jackrabbit = require("jackrabbit");
+
 const app = express();
 app.use(cors());
+
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -12,25 +16,39 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 const config = require("./config")[process.env.NODE_ENV || "development"];
 
 const log = config.log();
-const service = require("./service");
 
 const server = app.listen(process.env.PORT || 4000);
-app.use(service);
+
+const transporter = nodemailer.createTransport({
+	service: "Gmail",
+	auth: {
+		user: process.env.USER,
+		pass: process.env.PASSWORD,
+	},
+	tls: {
+		rejectUnauthorized: false,
+	},
+	ignoreTLS: true,
+	secure: false,
+	port: 587,
+});
 
 server.on("listening", () => {
+	log.info(`Hi there! I'm listening on port ${server.address().port}`);
+
 	const registerService = () =>
 		axios
 			.put(
-				`https://student-portal-serviceregistry.herokuapp.com/register/${config.name}/${config.version}/${
-					server.address().port
-				}`
+				`https://student-portal-serviceregistry.herokuapp.com/register/${
+					config.name
+				}/${config.version}/${server.address().port}`
 			)
 			.then((res) => log.debug(res.data));
 	const unregisterService = () =>
 		axios.delete(
-			`https://student-portal-serviceregistry.herokuapp.com/register/${config.name}/${config.version}/${
-				server.address().port
-			}`
+			`https://student-portal-serviceregistry.herokuapp.com/register/${
+				config.name
+			}/${config.version}/${server.address().port}`
 		);
 
 	registerService();
@@ -40,6 +58,30 @@ server.on("listening", () => {
 		clearInterval(interval);
 		await unregisterService();
 	};
+
+	const rabbit = jackrabbit(process.env.QUEUE_URL);
+	const exchange = rabbit.default();
+
+	const queue = exchange.queue({ name: "mail_queue", durable: true });
+	queue.consume(onMessage);
+
+	async function onMessage(data, ack) {
+		try {
+			let message = {
+				from: process.env.SENDER,
+				to: data.email,
+				subject: data.subject,
+				text: data.text,
+			};
+
+			await transporter.sendMail(message);
+			log.info(`Email sent to ${data.email} from ${process.env.SENDER}`);
+
+			ack();
+		} catch (err) {
+			log.error(err.message);
+		}
+	}
 
 	process.on("SIGINT", async () => {
 		await cleanup();
@@ -60,6 +102,4 @@ server.on("listening", () => {
 			await cleanup();
 			process.exit(0);
 		});
-
-	log.info(`Hi there! I'm listening on port ${server.address().port}`);
 });
